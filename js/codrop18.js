39681802a28195d82ee608c260836237647feaf8
Mok Jee Jin <jeejin@piktochart.com>
2016-09-14 23:36:21 +0800

    WIP codrop18

--- js/codrop18.js
+++ js/codrop18.js
@@ -10,7 +10,6 @@ $(function(){
         $imageBar.css('width', slideBarWidth  + 'px');
     }
 
-
     $('.tng-slider-bar').on('click', function(){
         var $bar = $(this);
 
@@ -22,4 +21,23 @@ $(function(){
             expandSlider($bar);
         }
     })
+
+    $('.tng-slider-images-wrapper').off('mousemove').on('mousemove', function(e){
+
+        var $extra          = 500;
+
+        var $this           = $(this);
+        var $outerWidth     = $(this).width();
+        var $innerWidth     = $(this).find('.tng-slider-images').width() + (2 * $extra);
+        var $widthDiff      = $innerWidth - $outerWidth;
+
+        // var $left           = e.pageX * (2.333);
+        var $left           = e.pageX * ($widthDiff/$outerWidth) - $extra;
+
+        $this.scrollLeft($left);
+
+        // console.log("e.pageX: " + e.pageX)
+        console.log("$widthDiff " + $widthDiff);
+        console.log("$outerWidth " + $outerWidth);
+    });
 });
