98a5e5eee9b1046d8960c6a099d9aff572491e2f
Mok Jee Jin <jeejin@piktochart.com>
2017-02-09 12:18:46 +0800

    Added Styleguide Interactive

---
+++ js/project_pikto_styleguide.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  
+  var current_active = $('.sg-btn-first');
+  var container = document.querySelector('.sg-main-container');
+  var sg_button_list_btn = document.querySelectorAll('.sg-button-list > li > button');
+  var sg_btn = document.querySelectorAll('.sg-btn');
+
+  // console.log(sg_button_list_btn);
+
+  /*Default*/
+  container.innerHTML = sg_data_logo.innerHTML;
+
+  function changeContent( ev ) {
+    // console.log( 'changeContent', ev);
+    const data = ev.currentTarget.dataset.active;
+    const target = document.getElementById(data);
+    ev.currentTarget.innerHTML = target.innerHTML;
+    ev.currentTarget.classList.remove('slide-out');
+  }
+
+  container.addEventListener( 'transitionend', changeContent );
+
+  function makeSlideOut( selector ) {
+    return function slideOut( ev ) {
+      // console.log('slideOut', ev);
+      const container = document.querySelector( selector );
+      container.dataset.active = ev.currentTarget.dataset.target;
+      container.classList.add('slide-out');
+    }
+  }
+
+  [...sg_button_list_btn].forEach( function(elem) {
+    elem.addEventListener('mousedown', makeSlideOut( '.sg-main-container' ) );
+    // console.log(elem);
+  });
+
+  // [...sg_btn].forEach( function(elem) {
+  //   elem.addEventListener('mousedown', function(){
+  //     $('.sg_btn').parent().addClass('active');
+  //     elem.parentElement.classList.add('active');
+  //   });
+  // });
+
+  $('.sg-btn').on('click', function(){
+    $('.sg-btn').closest('li').removeClass('active');
+    $(this.closest('li')).addClass('active');
+    current_active = this;
+  })
+});
+
+/*OLD*/
+// document.addEventListener("DOMContentLoaded", function(event) { 
+  
+//   var container = document.getElementsByClassName('sg-main-container')[0];
+
+//   /*Default*/
+//   container.innerHTML = sg_data_logo.innerHTML;
+
+//   $('button').on('click', function(){
+//     container.classList.add('slide-out');
+
+//     var data = this.getAttribute('data-target');
+//     var target = document.getElementById(data);
+//     container.innerHTML = target.innerHTML;
+
+//     container.classList.remove('slide-out');
+//   });
+
+// });
