d671d1c6168178fdbaaade83884cca162ebddacd
Mok Jee Jin <jeejin@piktochart.com>
2017-02-10 18:11:02 +0800

    Added clipboard, refine styleguide JS

--- js/project_pikto_styleguide.js
+++ js/project_pikto_styleguide.js
@@ -2,19 +2,70 @@ document.addEventListener("DOMContentLoaded", function(event) {
   
   var current_active = $('.sg-btn-first');
   var container = document.querySelector('.sg-main-container');
+  var sg_container_list = document.querySelectorAll('.sg-main-container > section');
   var sg_button_list_btn = document.querySelectorAll('.sg-button-list > li > button');
   var sg_btn = document.querySelectorAll('.sg-btn');
 
-  // console.log(sg_button_list_btn);
+  /*Instantiate clipboard*/
+  /*Clipboard uses a library call clipboard.min.js*/
+  var clipboard = new Clipboard('.sg-colorbox');
+  clipboard.on('success', function(e) {
+      console.log(e);
+  });
+  clipboard.on('error', function(e) {
+      console.log(e);
+  });
+
+  [...sg_button_list_btn].forEach( function(elem) {
+    elem.addEventListener('mousedown', function(){
+
+      /*Remove all active and slide-in class from */
+      [...sg_container_list].forEach( function(elem){
+        elem.classList.remove('active');
+        elem.classList.remove('slide-in');
+      });
+
+      var target = elem.getAttribute('data-target');
+      document.querySelector('#' + target).classList.add('active');
+      setTimeout(function(){
+        document.querySelector('#' + target).classList.add('slide-in');
+      }, 200)
+    });
+  });
+
+  /*Generate Background Color from HTML Content*/
+  $('.sg-colorbox').each(function(){
+    $(this).html(this.getAttribute('data-clipboard-text'));
+    $(this).css("background-color",this.getAttribute('data-clipboard-text'));
+  })
+
+  $('.sg-colorbox').on('click', function(){
+    $('.sg-color-copy-label').addClass('fade-in');
+    setTimeout(function(){
+      $('.sg-color-copy-label').removeClass('fade-in');
+    }, 500)
+  });
 
   /*Default*/
-  container.innerHTML = sg_data_website_colour.innerHTML;
+  $("button[data-target='sg_data_logo']").parent('li').addClass('active');
 
+  $('.sg-button-list--outer').find('li').on('click', function(){
+    $('.sg-button-list--outer').find('li').removeClass('active');
+    $(this).addClass('active');
+  });
+
+  $('.sg-button-list--inner').find('li').on('click', function(e){
+    $('.sg-button-list--inner').find('li').removeClass('active--inner');
+    $(this).addClass('active--inner');
+  })
+
+});
+
+/*
   function changeContent( ev ) {
     // console.log( 'changeContent', ev);
     const data = ev.currentTarget.dataset.active;
     const target = document.getElementById(data);
-    ev.currentTarget.innerHTML = target.innerHTML;
     ev.currentTarget.classList.remove('slide-out');
   }
 
@@ -31,32 +82,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   [...sg_button_list_btn].forEach( function(elem) {
     elem.addEventListener('mousedown', makeSlideOut( '.sg-main-container' ) );
-    // console.log(elem);
   });
-
-  // [...sg_btn].forEach( function(elem) {
-  //   elem.addEventListener('mousedown', function(){
-  //     $('.sg_btn').parent().addClass('active');
-  //     elem.parentElement.classList.add('active');
-  //   });
-  // });
-
-  // $('.sg-btn').on('click', function(){
-  //   $('.sg-btn').closest('li').removeClass('active');
-  //   $(this.closest('li')).addClass('active');
-  //   current_active = this;
-  // })
-
-  $('.sg-button-list--outer').find('li').on('click', function(){
-    $('.sg-button-list--outer').find('li').removeClass('active');
-    $(this).addClass('active');
-  });
-
-  $('.sg-button-list--inner').find('li').on('click', function(e){
-    $('.sg-button-list--outer').find('li').removeClass('active--inner');
-    $(this).addClass('active--inner');
-  })
-});
+*/
 
 /*OLD*/
 // document.addEventListener("DOMContentLoaded", function(event) { 
