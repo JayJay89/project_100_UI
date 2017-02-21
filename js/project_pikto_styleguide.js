2053bdc7f2cb20c73a596980335428c0f90d40b8
Mok Jee Jin <jeejin@piktochart.com>
2017-02-21 13:11:25 +0800

    Added new system for code implementation in styleguide

--- js/project_pikto_styleguide.js
+++ js/project_pikto_styleguide.js
@@ -4,8 +4,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var container = document.querySelector('.sg-main-container');
   var sg_container_list = document.querySelectorAll('.sg-main-container > section');
   var sg_button_list_btn = document.querySelectorAll('.sg-button-list > li > button');
-  var sg_btn = document.querySelectorAll('.sg-btn');
   var code_box = document.querySelectorAll('.sg-code-box');
+  var sg_content = document.querySelectorAll('.sg-content-box');
 
   /*Instantiate clipboard*/
   /*Clipboard uses a library call clipboard.min.js*/
@@ -25,6 +25,40 @@ document.addEventListener("DOMContentLoaded", function(event) {
   $("button[data-target='sg_data_alerts']").parent('li').addClass('active');
   $("#sg_data_alerts").addClass('active slide-in');
 
+  [...sg_content].forEach( function(elem){
+    var current_content = elem.innerHTML;
+    const target = elem.getAttribute('data-content');
+    const target_content = document.querySelector('#' + target);
+    const target_child = target_content.querySelector('.sg-code-syntax');
+
+    var mapObj = {
+      "<": "&lt;",
+      ">": "&gt;",
+      /*This replaces the first newline of the code with nothing*/
+      "\n":"",
+      /*This removes empty white spaces*/
+      "  ":""
+    };
+
+    /*Regex Replacement */
+    current_content = current_content.replace(/<|>|  /gi, function(matched){
+      return mapObj[matched];
+    }).replace(/\n/i, function(matched){
+      /*Only the first new line needs to be remove, hence the global tag is omitted*/
+      return mapObj[matched];
+    });
+
+    const line_count = current_content.match(/\n/g).length;
+
+    console.log("Count: ",line_count);
+    target_child.innerHTML = current_content;
+    
+    // console.log(target_content);
+    // console.log(target_child);
+  });
+
+
+
   /*Initialize highlight.js*/
   // $(document).ready(function() {
   //   $('pre code').each(function(i, block) {
@@ -33,19 +67,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // });
 
   /*Number Counter System*/
-  [...code_box].forEach( function(elem) {
-    var pre_code = elem.querySelector('.sg-code-syntax');
-    var side_bar = elem.querySelector('.sg-code-box-sidebar');
+  /*When an element is not rendered on the page, we can't get the height value of the element. Hence, some elements doesn't render the numbers.*/
+  // [...code_box].forEach( function(elem) {
+  //   var pre_code = elem.querySelector('.sg-code-syntax');
+  //   var side_bar = elem.querySelector('.sg-code-box-sidebar');
 
-    var style = window.getComputedStyle(pre_code);
-    var line_height = parseInt(style.getPropertyValue('line-height'), 10);
-    var numLines = parseInt($( pre_code ).height(), 10)/(line_height);
+  //   var style = window.getComputedStyle(pre_code);
+  //   var line_height = parseInt(style.getPropertyValue('line-height'), 10);
+  //   var numLines = parseInt($( pre_code ).height(), 10)/(line_height);
 
-    console.log( style.getPropertyValue('height') );
-    for(i = 1; i <= numLines; i++) {
-      side_bar.innerHTML += ("<span>" + i + "</span>");
-    }
-  });
+  //   console.log( style.getPropertyValue('height') );
+  //   for(i = 1; i <= numLines; i++) {
+  //     side_bar.innerHTML += ("<span>" + i + "</span>");
+  //   }
+  // });
 
   [...sg_button_list_btn].forEach( function(elem) {
     elem.addEventListener('mousedown', function(){
@@ -89,8 +124,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 
-
-/*
+/* Old Codes
   function changeContent( ev ) {
     // console.log( 'changeContent', ev);
     const data = ev.currentTarget.dataset.active;
