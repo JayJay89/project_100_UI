f20c2b050b76ff89b74766578da6214f99502454
Mok Jee Jin <jeejin@piktochart.com>
2017-03-29 10:22:14 +0800

    Added Animation Prototypes

--- js/js_animation_playground.js
+++ js/js_animation_playground.js
@@ -17,32 +17,93 @@ js_counter.addEventListener('mousedown', function(){
 
 js_box.addEventListener('click', function(e){
   console.log("move");
-  moveBy(e.target, 270, 1);
+  // moveBy(e.target, 500, 1);
+  // moveBy2(e.target, "left", "px", 0 , 500, 2000);
+  moveBy3(e.target);
 })
 
-/*http://javax3script.info/js-animation*/
-var moveBy = function(target, distance, seconds){
+/*http://javascript.info/css-animations*/
+/*Prototype 1*/
+/*It works, but timing and move distance was inaccurate */
+var moveBy = function(movetarget, distance, duration){
   
+  var start = Date.now();
+  var fps = 20;
   var pos = 0;
-  var toPosition = distance;
-  const fps = 1;
-  var time =  seconds * 1000;
+  var time =  duration * 1000;
 
   var moveInterval = setInterval(moveTarget, fps);
 
   function moveTarget(){
-    if (pos == toPosition) {
-      clearInterval(moveInterval);
-    } else {
-      pos += (distance/time);
-      console.log(pos);
-      target.style.left = pos + 'px';
-    }
-  }
+    let timePassed = Date.now() - start;
 
-  /*The answer is to calculate the time passed, rather than how far this guy reach*/
+    if (timePassed >= time) {
+      clearInterval(moveInterval);
+      console.log("clear!");
+      return;
+    }
+
+    pos += (distance * fps)/time ;
+    movetarget.style.left = pos;
+  }
 }
 
+/*http://stackoverflow.com/questions/11213259/javascript-animation*/
+/*Prototype 2*/
+/*This one works well, accurate results as well */
+var moveBy2 = function(elem, style, unit, from, to, time){
 
+  var start = Date.now();
+  var timer = setInterval(moveTarget, 25);
+
+  function moveTarget(){
+    var step = Math.min(1,(new Date().getTime()-start)/time);
+    var step2 = Date.now() - start;
+    
+    console.log("step " + step);
+    var distance = to - from;
+
+    elem.style[style] = (from + step * distance ) + unit;
+
+    /*clear interval*/
+    if( step == 1) clearInterval(timer);
+  }
+
+  elem.style[style] = from + unit;
+}
+
+// http://javascript.info/js-animation#setinterval
+/*Prototype 3*/
+var moveBy3 = function(target){
+  let start = performance.now();
+  var duration = 500;
+  var distance = 300;
+
+  requestAnimationFrame(animate);
+
+  function animate(){
+    let timePassed = performance.now() - start;
+    let progress = timePassed / duration;
+
+    if (progress > 1) {
+      progress = 1;
+    }
+
+    moveLeft();
+
+    if (progress == 1) {
+      cancelAnimationFrame(animate);
+    } else {
+      requestAnimationFrame(animate);
+    }
+
+    function moveLeft(){
+      target.style.left = (distance * progress) + "px"; 
+      console.log("timePassed " + timePassed);
+      console.log("left " + target.style.left);
+      console.log("progress " + progress);
+    }
+  }
+}
 
 
