ed5c904edec7ce999e7a3da8cb9088b22ae73b5d
Mok Jee Jin <jeejin@piktochart.com>
2017-05-28 22:43:22 +0800

    styleguide stuff

--- js/project_pikto_styleguide.js
+++ js/project_pikto_styleguide.js
@@ -38,8 +38,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
   });
 
   /*INIT*/
-  $("button[data-target='sg_data_button']").parent('li').addClass('active');
-  $("#sg_data_button").addClass('active slide-in');
+  $("button[data-target='sg_data_selects']").parent('li').addClass('active');
+  $("#sg_data_selects").addClass('active slide-in');
 
   function injectCode (contentbox){
     var current_content = contentbox.innerHTML;
